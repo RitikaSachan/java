1323dd286f2e0750ff6ac269b9dcd131f3ea7966
RitikaSachan <RITIKASACHAN99@GMAIL.COM>
2025-05-14 15:10:10 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sr=new Scanner(System.in);
+        System.out.println("give the n:");
+        int a=sr.nextInt();
+        
+        for(int i=0;i<=10;i++){
+             System.out.println(a+" x "+i+"="+a*i);
+        }
+    }
+}
