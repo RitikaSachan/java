70ae2f3e5ff7c81a37cbdd36c765e82635d5e789
RitikaSachan <RITIKASACHAN99@GMAIL.COM>
2025-05-30 01:06:01 +0530

    Create hollow_rhombus.java

---
+++ hollow_rhombus.java
@@ -0,0 +1,26 @@
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sr=new Scanner(System.in);
+        System.out.println("enter the value of n: ");
+        int n=sr.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+             for(int k=1;k<=n;k++){
+                if(i==1 || i==n || k==1 || k==n ){
+                System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+             }
+             System.out.println();
+        }
+    }
+}
+
