a726e0149bba52c29ffd180f3c15cb9defbb8c17
RitikaSachan <RITIKASACHAN99@GMAIL.COM>
2025-05-30 00:48:28 +0530

    Create floyd's triangle.java

---
+++ floyd's triangle.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int p=1;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(p +" ");
+                p++;
+                
+            }
+             System.out.println();
+        }
+       
+    //  System.out.println();
+        
+    }
+}
+
