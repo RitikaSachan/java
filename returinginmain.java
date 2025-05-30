ae5efad3e1bbcb855a299fc32204f7bc36be33a3
RitikaSachan <RITIKASACHAN99@GMAIL.COM>
2025-05-31 01:13:49 +0530

    Create returinginmain.java

---
+++ returinginmain.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static int avg(int a,int b,int c) {
+         int average=(a+b+c)/3;
+         return average;
+    }
+    public static void main(String[] args) {
+      int a=20;
+      int b=30;
+      int c=40;
+      int avgg=avg(a,b,c);
+      System.out.print(avgg);
+    }
+}
