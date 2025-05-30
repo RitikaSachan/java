2b73cbce9e6ae7989d4fdee6ec9a787938692c2b
RitikaSachan <RITIKASACHAN99@GMAIL.COM>
2025-05-31 01:19:19 +0530

    Create f_to_c(in mainfunctionwithvoid).java

---
+++ f_to_c(in mainfunctionwithvoid).java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static float avg(float c) {
+         float f=(9/5)*c+32;;
+         return f;
+    }
+    public static void main(String[] args) {
+      float c=40;
+      float ff=avg(c);
+      System.out.print(ff);
+    }
+}
